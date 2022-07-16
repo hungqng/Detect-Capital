bbfc5d35d4705c8cbefeb743be8aeba9c91d7739
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-07-16 10:15:59 -0700

    Add files via upload

--- Detect Capital.py
+++ Detect Capital.py
@@ -10,3 +10,10 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         return word.isupper() or word.islower() or word.istitle()
+
+        # Solution 2
+        cnt = sum(c.isupper() for c in word)
+
+        return cnt == len(word) \
+            or cnt == 0 \
+            or cnt == 1 and word[0].isupper()
